75d8ca91c4a9157e78b7f66ee4be605f9906ec18
Skorzynski <Kamil.Skorzynski@jacobs.com>
2020-04-20 12:17:33 +0200

    delete whitespaces and comments

--- Wallet.js
+++ Wallet.js
@@ -1,33 +1,26 @@
 class Wallet {
-    constructor(money) {
-        let _money = money;
-        //pobieranie aktualnej zawartosci portfela
-        this.getWalletValue = () => _money;
+  constructor(money) {
+    let _money = money;
 
-        // Sprawdzanie czy użytkownik ma odpowiednią ilość środków
-        this.checkCanPlay = (value) => {
-            if (_money >= value) return true;
-            return false
+    this.getWalletValue = () => _money;
+
+    this.checkCanPlay = value => {
+      if (_money >= value) return true;
+      return false;
+    };
+    this.changeWallet = (value, type = "+") => {
+      if (typeof value === "number" && !isNaN(value)) {
+        if (type === "+") {
+          return (_money += value);
+        } else if (type === "-") {
+          return (_money -= value);
+        } else {
+          throw new Error("Nieprawidłowy typ działania");
         }
-        this.changeWallet = (value, type = "+") => {
-            if (typeof value === "number" && !isNaN(value)) {
-                if (type === "+") {
-                    return _money += value
-                } else if (type === "-") {
-                    return _money -= value;
-                } else {
-                    throw new Error('Nieprawidłowy typ działania')
-                }
-            } else {
-                console.log(typeof value);
-                throw new Error("Nieprawidłowe wprowadzenie")
-            }
-        }
-
-
-
-    }
-
+      } else {
+        console.log(typeof value);
+        throw new Error("Nieprawidłowe wprowadzenie");
+      }
+    };
+  }
 }
-
-// const wallet = new Wallet(200)
